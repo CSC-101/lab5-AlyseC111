3f64a9c42ec85ef361a6ae38c8491d59e419a223
AlyseC111 <alcourtn@calpoly.edu>
2024-10-23 13:53:29 -0700

    Lab 5 Submission

--- lab5.py
+++ lab5.py
@@ -1,22 +1,53 @@
-import data
-
+from data import *
 # Write your functions for each part in the space below.
 
 # Part 1
    # The function for Part 1 should be within the class in data.py.
 
-
 # Part 2
    # The function for Part 2 should be within the class in data.py.
-
-
 # Part 3
-
+def time_add(time1: Time, time2: Time) -> Time:
+    hours = time1.hour + time2.hour
+    minutes = time1.minute + time2.minute
+    seconds = time1.second + time2.second
+    if seconds > 59:
+        seconds -= 59
+        minutes += 1
+    if minutes > 59:
+        minutes -= 59
+        hours += 1
+    return Time(hours, minutes, seconds)
 
 # Part 4
-
+def is_descending(list1: list[float]) -> bool:
+    for i in range(len(list1) - 1):
+        if list1[i] < list1[i+1]:
+            return False
+        else:
+            return True
+# [3, 2, 1]
 
 # Part 5
-
+def largest_between(list: list[int], lower: int, upper: int) -> any:
+    if lower > upper:
+        return None
+    elif lower < 0 or upper > len(list) - 1:
+        return None
+    largest = lower
+    for i in range(lower, upper):
+        if list[i] > list[largest]:
+            largest = i
+    return largest
 
 # Part 6
+def furthest_from_origin(l: list[Point]) -> any:
+    if len(l) == 0:
+        return None
+    greatest = round(math.sqrt((l[0].x-0)**2 + (l[0].y-0)**2), 2)
+    index = 0
+    for p in l:
+        if round(math.sqrt((p.x-0)**2 + (p.y-0)**2), 2) > greatest:
+            greatest = round(math.sqrt((p.x-0)**2 + (p.y-0)**2), 2)
+            index = l.index(p)
+    return index
--- lab5_tests.py
+++ lab5_tests.py
@@ -14,17 +14,57 @@ class TestCases(unittest.TestCase):
 
 
     # Part 3
-
-
+    def test_time_add_1(self):
+        time1 = data.Time(4, 9, 23)
+        time2 = data.Time(9, 35, 40)
+        expected = data.Time(13, 45, 4)
+        actual = lab5.time_add(time1, time2)
+        self.assertEqual(expected, actual)
+    def test_time_add_2(self):
+        time1 = data.Time(15, 35, 10)
+        time2 = data.Time(0, 48, 40)
+        expected = data.Time(16, 24, 50)
+        actual = lab5.time_add(time1, time2)
+        self.assertEqual(expected, actual)
     # Part 4
-
+    def test_is_descending_1(self):
+        list = [2, 5, 1, 9, 0, 3]
+        expected = False
+        actual = lab5.is_descending(list)
+        self.assertEqual(expected, actual)
+    def test_is_descending_2(self):
+        list = [10.3, 9.2, 8.0, 7.4, 6.3, 5.0]
+        expected = True
+        actual = lab5.is_descending(list)
+        self.assertEqual(expected, actual)
 
     # Part 5
-
-
+    def test_largest_between_1(self):
+        list = [4, 7, 3, 7, 1, 6, 3, 10, 2, 4]
+        expected = 7
+        actual = lab5.largest_between(list, 2, 8)
+        self.assertEqual(expected, actual)
+    def test_largest_between_2(self):
+        list = [8, 4, 3, 6, 7]
+        expected = None
+        actual = lab5.largest_between(list, 1, 5)
+        self.assertEqual(expected, actual)
     # Part 6
-
-
+    def test_furthest_from_origin_1(self):
+        list = [data.Point(10, 3), data.Point(9.2, 8.0), data.Point(7.4, 6.3), data.Point(5, 0)]
+        expected = 1
+        actual = lab5.furthest_from_origin(list)
+        self.assertEqual(expected, actual)
+    def test_furthest_from_origin_2(self):
+        list = [data.Point(23, 78), data.Point(3, 5), data.Point(2, 93), data.Point(24, 13)]
+        expected = 2
+        actual = lab5.furthest_from_origin(list)
+        self.assertEqual(expected, actual)
+    def test_furthest_from_origin_3(self):
+        list = []
+        expected = None
+        actual = lab5.furthest_from_origin(list)
+        self.assertEqual(expected, actual)
 
 
 
